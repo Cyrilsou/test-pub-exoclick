9afc88a248d01af0867c954e36a2a20872fb0741
Cyrilsou <96681831+Cyrilsou@users.noreply.github.com>
2025-06-22 10:17:40 +0200

    Delete SITE POUR TESTER LES PUBS directory

--- SITE POUR TESTER LES PUBS/script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-console.log("Le site de test est prêt !");
-
-// Vous pourrez ajouter ici des scripts plus complexes pour le test de publicités,
-// par exemple pour charger des publicités de manière dynamique ou pour simuler
-// le comportement d'un utilisateur. 
