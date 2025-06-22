92ded8898356e7e5cb0ce6795fcc1779d8d827ca
Cyrilsou <96681831+Cyrilsou@users.noreply.github.com>
2025-06-22 10:17:18 +0200

    Add files via upload

---
+++ SITE POUR TESTER LES PUBS/script.js
@@ -0,0 +1,6 @@
+// script.js
+console.log("Le site de test est prêt !");
+
+// Vous pourrez ajouter ici des scripts plus complexes pour le test de publicités,
+// par exemple pour charger des publicités de manière dynamique ou pour simuler
+// le comportement d'un utilisateur. 
